bcaba077e7567891e530f257ce4b28801e0d5f93
Blake Tucker <brtucker@asu.edu>
2013-12-21 12:00:51 -0700

    Completed Problem 4

--- Project4.c
+++ Project4.c
@@ -7,3 +7,45 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+
+char* strrev(char* s) {
+    char *s1, *s2;
+    char c;
+    
+    s1 = s2 = s;
+    while (*s2)
+        s2++;
+    
+    while (s1 < s2) {
+        c = *(--s2);
+        *s2 = *s1;
+        *s1++ = c;
+    }
+    return s;
+}
+
+int main(){
+    int index, innerIndex;
+    int largest = -1;
+    char s1[7];
+    char s2[7];
+    
+    for (index = 100; index < 1000; ++index) {
+        for (innerIndex = index; innerIndex < 1000; ++innerIndex) {
+            int sum = index * innerIndex;
+            sprintf(s1, "%d",sum);
+            strcpy(s2, s1);
+            strrev(s2);
+            if (strcmp(s1, s2) == 0 && sum > largest) {
+                largest = sum;
+            }
+        }
+    }
+    
+    printf("%d\n", largest);
+    
+    return 0;
+}
